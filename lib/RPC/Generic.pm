e1690b4959e46cf4f9a6e4a6bed0e84db70e6c33
Randy Stauner <randy@magnificent-tears.com>
2010-10-12 11:53:01 -0700

    Use RPC::Generic::Message instead of plain hashes
    Define _message_class() to try to find the appropriate ::Message class to use.

--- lib/RPC/Generic.pm
+++ lib/RPC/Generic.pm
@@ -50,6 +50,35 @@ Return a list (hash) of default options to be used at initialization.
 
 sub _defaults { () }
 
+=method _message_class
+
+Return class name (string) for creating message objects.
+Will attempt to load a module by appending '::Message'
+to the current [sub]class.
+If not found, returns L<RPC::Generic::Message>.
+
+=cut
+
+# TODO: search all @ISA's ?
+
+sub _message_class {
+	my ($self) = @_;
+	# cache the result
+	$self->{message_class} ||= do {
+		# try subclass::Message, fall back to __PACKAGE__::Message
+		my $class = ref($self);
+		my $message_class = eval {
+			foreach my $base ( $class, __PACKAGE__ ){
+				my $mod = "${base}::Message";
+				return $mod if eval "require $mod;";
+			}
+		};
+		# call as function rather than object method because $class does not inherit from Serializer
+		RPC::Generic::Serializer::copy_methods_to_namespace($class, $message_class);
+		$message_class; # return
+	};
+}
+
 =method _rpc METHOD,PARAMETERS
 
 Generic method for requesting the server to run I<METHOD> with provided I<PARAMETERS>.
@@ -62,14 +91,16 @@ sub _rpc {
 	my $response;
 	my $request = $self->_rpc_request($method, @params);
 	eval {
-		$remote->print($self->_serialize($request));
+		# explicitly stringify in case remote doesn't
+		$remote->print("$request");
 		local $/;
-		$response = $self->_deserialize($remote->getline());
+		$response = $self->_rpc_response(<$remote>);
 	};
 	if(my $e = $@){
 		$response = $self->_rpc_response($request, undef, $self->_rpc_error($e));
 	}
-	return $response;
+	# return just the data since this is a simple, generic interface
+	return $response->data;
 }
 
 =method _rpc_error EXPR
@@ -98,24 +129,25 @@ sub _rpc_id {
 =method _rpc_request METHOD,PARAMETERS
 
 Return an RPC request object for the given I<METHOD> and I<PARAMETERS>.
+See L<RPC::Generic::Message/request>.
 
 =cut
 
 sub _rpc_request {
-	my ($self, $method, @params) = @_;
-	return {id => $self->_rpc_id, method => $method, params => [@params]};
+	my $self = shift;
+	return $self->_message_class->request($self->_rpc_id, @_);
 }
 
 =method _rpc_response ID,RESULT,ERROR
 
 Return an RPC response object.
+See L<RPC::Generic::Message/response>.
 
 =cut
 
 sub _rpc_response {
-	my ($self, $id, $result, $error) = @_;
-	# allow $id to be a rpc_request hash
-	return {id => (ref($id) ? $id->{id} : $id), result => $result, error => $error};
+	my $self = shift;
+	return $self->_message_class->response(@_);
 }
 
 # NOTE: no need for generic DESTROY; it will be called automatically on sub-objects
